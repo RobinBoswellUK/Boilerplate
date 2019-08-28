52fb82191d5f2caa0efa666430d8e574b3b5491d
doctorbobapplications <doctorbob567@gmail.com>
2019-08-28 16:04:50 +0100

    added the service worker messagechannel demo files

---
+++ service-worker-demo-messages/sw.js
@@ -0,0 +1,59 @@
+self.addEventListener('message', function(event) {
+    // assume the message is the JSON format type and has a port
+    incomingCommand = event.data.command
+    incomingMessage = event.data.message
+    loopBackPort    = event.ports[0]
+          //service workers close and reopen so there is no convenient way
+          //to permenantly save and reuse the port. 
+    
+    // if something is undefined assume it is a simple string message
+    if(undefined==incomingCommand|undefined==incomingMessage){
+        incomingMessage = event.data
+        console.log("SW message - ",incomingMessage);
+        return
+    }
+        
+    // JSON with command and message
+    if (incomingCommand == "simple") {
+        console.log("SW message - simple : ",incomingMessage);
+        return
+    }
+    
+    // JSON with command and message, and use the port to post back on    
+    if (incomingCommand == "loopback") {
+        console.log("SW message - loopback function new: ", incomingMessage);
+        loopBackPort.postMessage({
+            "message": "SW copy \' "+incomingMessage+" \' "
+        });
+        return
+    }    
+    console.log("SW message - unhandled type: "+incomingCommand)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
